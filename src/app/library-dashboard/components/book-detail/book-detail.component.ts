dd827a40b06ccca240dd90c25b514abe7730c6ef
livia <livia.mano@uni-konstanz.de>
2021-03-19 11:41:05 +0100

    Task 4: added book detail

--- src/app/library-dashboard/components/book-detail/book-detail.component.ts
+++ src/app/library-dashboard/components/book-detail/book-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BookServiceService } from 'src/app/book-service.service';
+import { Book } from '../../models/book.interface';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.scss']
 })
-export class BookDetailComponent  {
-  
+export class BookDetailComponent {
+
+  @Input() book: Book;
+
+  constructor(
+    private bookService: BookServiceService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.getBook();
+  }
+
+  getBook() {
+    let isbn = this.route.snapshot.paramMap.get('isbn');
+    console.log(isbn);
+    this.bookService.getBook(isbn).subscribe(book => this.book = book);
+  }
 }
